d4d7b7e9186387c93e612386af58d615279e9fa0
yedhink <yedhin1998@gmail.com>
2018-04-23 16:09:40 +0530

    Use synchronization

---
+++ Threads/BasicThreadsBySync.java
@@ -0,0 +1,54 @@
+package classExamples;
+public class BasicThreadsBySync {
+	
+	// Initializing count as zero
+	public static int count = 0;
+	
+	public static synchronized void countMethod() {
+		++count;
+	}
+
+	public static void main(String[] args) {
+		
+		// Thread 1
+		Thread t1 = new Thread ( new Runnable() {
+			public void run() {
+				for ( int i=0; i<10000; ++i){
+					// Statement to be clarified.
+					// System.out.println(count++);
+					// Simply count++ without print statement yields Final count as a random value 
+					// count++;
+					countMethod();
+				}
+			}
+		});
+		
+		// Thread 2
+		Thread t2 = new Thread ( new Runnable() {
+			public void run() {
+				for ( int i=0; i<10000; ++i){
+					// Statement to be clarified.
+					// System.out.println(count++);
+					// Simply count++ without print statement yields Final count as a random value 
+					// count++;
+					countMethod();
+				}
+			}
+		});
+		
+		t1.start();
+		t2.start();
+	    
+		// Wait till completion of both threads	
+		try{
+			t1.join();
+			t2.join();
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+
+		// Final Count
+		System.out.println("Final count : "+count);
+	}
+}
