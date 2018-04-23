4e67986d398204698c3c903a660163f230cd2f3f
yedhink <yedhin1998@gmail.com>
2018-04-23 16:06:49 +0530

    Create Runnable in main

---
+++ Threads/BasicThreadsByMainOnly.java
@@ -0,0 +1,21 @@
+package classExamples;
+public class BasicThreadsByMainOnly {
+	public static void main(String[] args) {
+		Thread obj1 = new Thread (new Runnable(){
+			public void run(){
+				for (int i = 0; i<10 ; ++i) {
+					System.out.println(Thread.currentThread().getId() + " shows "+i);
+					try {
+						Thread.sleep(100);
+					} catch(Exception e){
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+		);//runnable method ends here
+		obj1.start();
+	}
+}
+
+
