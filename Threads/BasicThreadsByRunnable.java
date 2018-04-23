3037a8376f86ed8bdc41b738fcac5523a7950259
yedhink <yedhin1998@gmail.com>
2018-04-23 15:58:27 +0530

    Create Threads

---
+++ Threads/BasicThreadsByRunnable.java
@@ -0,0 +1,24 @@
+package classExamples;
+class thread implements Runnable {
+	public void run(){
+		for (int i = 0; i<10 ; ++i) {
+			System.out.println(Thread.currentThread().getId() + " shows "+i);
+			try {
+				Thread.sleep(100);
+			} catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class BasicThreadsByRunnable {
+	public static void main(String[] args) {
+		Thread obj1 = new Thread(new thread());
+		Thread obj2 = new Thread(new thread());
+		Thread obj3 = new Thread(new thread());
+		obj1.start(); 	
+		obj2.start();
+		obj3.start();
+	}
+}
