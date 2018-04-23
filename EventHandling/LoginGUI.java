352543c0f196500a03a3ea5b3bf123fa000294a6
yedhink <yedhin1998@gmail.com>
2018-04-23 21:32:39 +0530

    Update login button

--- EventHandling/LoginGUI.java
+++ EventHandling/LoginGUI.java
@@ -2,45 +2,94 @@ package GUIProjects;
 import java.awt.TextField;
 import java.awt.*;
 import java.awt.Frame;
+import java.awt.Panel;
+import java.awt.Button;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JOptionPane;
-class AppletFrameActions extends Frame {
+
+class LoginGUI extends Frame {
+	// declare variables
 	private Label label1,label2; 
 	private TextField txtF1;
 	private TextField passF;
+	private Button buttonExit,buttonLogin;
+	private Panel panelExit,panelLogin;
+
 	// Looking for a better way to store this.
 	private final String uid = "Yedhin";
 	private final String pass = "*123#";
-
+	
+	// variables to know whether user inputted username and password is correct or not
 	private int flagUid = 0, flagPass = 0;
-	public AppletFrameActions(){
-		super("EventHandling using Frame class of applet");
-		setLayout(new FlowLayout());
-		label1 = new Label();
-		label1.setAlignment(Label.CENTER);
-		label1.setText("Username");
-		add(label1);
+	
+	// Constructor for the class
+	public LoginGUI(){
+		// set title
+		super("EventHandling basics");
 		txtF1 = new TextField();
 		passF = new TextField();
+		label1 = new Label();
+		label2 = new Label();
+		panelExit = new Panel();
+		panelLogin = new Panel();
+		buttonExit = new Button("close");
+		buttonLogin = new Button("login");
+
+		// explictely setting layout. default is Flow itself
+		setLayout(new FlowLayout());
+		
+		
+		//add(buttonExit);
+		// create a label for username
+		label1.setAlignment(Label.CENTER);
+		label1.setText("Username");
+		add(label1); // add the label to the Frame
+
+			
+		// set initial width of the the TextFields
 		txtF1.setColumns(20);
 		passF.setColumns(20);
+
+		// changing echoing of characters typed in password field into *
 		passF.setEchoChar('*');
+		
 		add(txtF1);
-		label2 = new Label();
+
+		// create a label for password
 		label2.setAlignment(Label.CENTER);
 		label2.setText("Password");
 		add(label2);
+
 		add(passF);
+		
+		// create an exit button
+		add(buttonExit);
+
+		// login button
+		//panelLogin.setLayout(null);
+		panelLogin.setBounds(100,100,100,50);
+		panelLogin.add(buttonLogin);
+		add(panelLogin);
+
+		// creating an listener object.
 		theHandler handler = new theHandler();
 		txtF1.addActionListener(handler);
 		passF.addActionListener(handler);
+		buttonExit.addActionListener(handler);
 	}
+
+	// EventHandling class
 	private class theHandler implements ActionListener {
 		public void actionPerformed(ActionEvent e) {
+			// Event on close button
+			if( e.getSource() == buttonExit ) {
+				dispose();
+				System.exit(0);
+			}
 			// Event in textField
-			if(e.getSource() == txtF1) {
+			else if(e.getSource() == txtF1) {
 				if(e.getActionCommand().equals(uid))
 				{
 					flagUid = 1;
@@ -71,7 +120,7 @@ class AppletFrameActions extends Frame {
 		}
 	}
 	public static void main(String[] args) {
-		AppletFrameActions frameObj = new AppletFrameActions();
+		LoginGUI frameObj = new LoginGUI();
 		frameObj.setSize(600,600);
 		frameObj.setVisible(true);	
 	}
