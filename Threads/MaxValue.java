65fef5c1cf803bb6ddca03d09057d9d56fc3a54d
yedhink <yedhin1998@gmail.com>
2018-04-23 16:16:38 +0530

    Find Maximum Value In Array

---
+++ Threads/MaxValue.java
@@ -0,0 +1,45 @@
+// Find the max value in an array of ints using 4 threads
+package multithreadingexamples;
+import java.util.*;
+class Largest extends Thread {
+	int[] arr;
+	int lo,hi;
+    long large;
+	Largest(int[] arr, int lo, int hi, long large){
+		this.arr = arr;
+		this.lo = lo;
+		this.hi = hi;
+		this.large = large;
+	}
+ 	public void run(){
+		for (int i=lo; i<hi; ++i) {
+			if (arr[i] > large) {
+				large = arr[i];	
+			}
+		}
+	}
+	public long getLargestInThread(){
+		return large;
+	}
+}
+
+public class MaxValue {
+	public static void main(String[] args) throws InterruptedException{
+		int[] arr = new int[1000*1000];
+		long maximum = 0;
+		Random rnd = new Random();
+		for (int i =0; i<arr.length; ++i) {
+			arr[i] = rnd.nextInt();
+			//System.out.println(arr[i]);
+		}
+		maximum = arr[0];
+		Largest[] t = new Largest[4];
+		for (int i=0; i<4; ++i) {
+			t[i] = new Largest(arr, i*arr.length/4, (i+1)*arr.length/4, maximum);
+			t[i].start();
+			t[i].join();
+			maximum = t[i].getLargestInThread();
+		}
+		System.out.println("Maximum value in array is "+maximum);
+	}	
+}
