c66b5b9e61da14ae32427a657dfa4b1117aa002e
yedhink <yedhin1998@gmail.com>
2018-04-25 15:12:32 +0530

    Main Execution

--- EasyKtuMath/Main.java
+++ EasyKtuMath/Main.java
@@ -6,14 +6,19 @@ public class Main {
 	public static void main(String[] args) throws IOException{
 		// create character stream object
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int choice;
+		String proceed = "n";
 		System.out.println("1)Trapezoidal Integration\n2)Simpsons Integration");
-		
+
 		// Enter which Method of Integration you want
-		System.out.print("Enter choice = ");
-		int choice = Integer.parseInt(br.readLine());
-		switch(choice){
-			case 1: NumericIntegration.trapezoidalIntegration();break;
-			case 2: NumericIntegration.simpsonsIntegration();break;
-		}
+		do{
+			System.out.print("Enter choice = ");
+			choice = Integer.parseInt(br.readLine());
+			switch(choice){
+				case 1: NumericIntegration.trapezoidalIntegration();break;
+				case 2: NumericIntegration.simpsonsIntegration();break;
+			}
+			System.out.print("Continue?(y/n) :- ");
+		}while(br.readLine().equalsIgnoreCase("y"));
 	}	
 }
