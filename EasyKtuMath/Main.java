6dca3a8689afee5d79acdc4583a83efcc64d8de2
yedhink <yedhin1998@gmail.com>
2018-04-25 11:38:17 +0530

    Add comments

--- EasyKtuMath/Main.java
+++ EasyKtuMath/Main.java
@@ -4,8 +4,11 @@ import java.io.*;
 import java.lang.*;
 public class Main {
 	public static void main(String[] args) throws IOException{
+		// create character stream object
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("1)Trapezoidal Integration\n2)Simpsons Integration");
+		
+		// Enter which Method of Integration you want
 		System.out.print("Enter choice = ");
 		int choice = Integer.parseInt(br.readLine());
 		switch(choice){
