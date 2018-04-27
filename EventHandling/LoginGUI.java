c27861bfa06eb55a54b4913f6bc9a62600454516
yedhink <yedhin1998@gmail.com>
2018-04-27 08:33:04 +0530

    Create A Login GUI

--- EventHandling/LoginGUI.java
+++ EventHandling/LoginGUI.java
@@ -1,3 +1,14 @@
+/*
+ * @name        A Login GUI 
+ * @package     GUIProjects
+ * @file        LoginGUI.java
+ * @author      Yedhin Kizhakkethara
+ * @email       yedhin1998@gmail.com
+ * @link        https://github.com/yedhink/KTU-Java
+ * @copyright   Copyright (c) 2018 ikigai, All Rights Reserved.
+ * @license     MIT
+ * @create      23-04-2018
+ */
 package GUIProjects;
 import java.awt.TextField;
 import java.awt.*;
