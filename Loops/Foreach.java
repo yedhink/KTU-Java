b5df248a3e80725cce25f51f3860751d497dfffd
kurianbenoy <kurian.pro@gmail.com>
2018-05-04 00:11:48 +0530

    Add For Each loop program also

---
+++ Loops/Foreach.java
@@ -0,0 +1,18 @@
+/* A program to find average of nos 1,4,2,6,7
+ * Using For Each loops
+ * */
+
+class Foreach 
+{
+	public static void main(String args[]){
+		 int sum=0,av;
+		 int avg [] = {1,4,2,6,7};
+		 for(int i : avg){
+			 sum +=i;
+
+		 }
+		 av = sum/avg.length;
+		 System.out.println("Average is "+av);
+
+	}
+}
