c983dfb4491ff860e4c4e11288dae3854fedc5a3
yedhink <yedhin1998@gmail.com>
2018-05-04 10:56:35 +0530

    Update IfElse

--- Loops/ifelse.java
+++ Loops/ifelse.java
@@ -2,36 +2,37 @@
  * https://www.hackerrank.com/challenges/java-if-else/problem
  */
 
-    import java.io.*;
-    import java.util.*;
-    import java.text.*;
-    import java.math.*;
-    import java.util.regex.*;
+package loops;
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
-    public class ifelse {
+public class ifelse {
 
-        public static void main(String[] args) {
+	public static void main(String[] args) {
 
-            Scanner sc=new Scanner(System.in);
-            int n=sc.nextInt();
-            String ans="";
-            if(n%2==1){
-              ans = "Weird";
-            }
-            else{
-                if(n>20){
-                    ans = "Not Weird";
-                }
-                if(n>=2&&n<5){
-                    ans = "Not Weird";
-                }
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String ans="";
+		if(n%2==1){
+			ans = "Weird";
+		}
+		else{
+			if(n>20){
+				ans = "Not Weird";
+			}
+			if(n>=2&&n<5){
+				ans = "Not Weird";
+			}
 
-                if(n>=6&&n<=20){
-                    ans = "Weird";
-                }
-            }
-            System.out.println(ans);
+			if(n>=6&&n<=20){
+				ans = "Weird";
+			}
+		}
+		System.out.println(ans);
 
-        }
-    }
+	}
+}
 
