c54676837ce4b7e4e8ccfff8c196dadfc75cb582
yedhink <yedhin1998@gmail.com>
2018-05-04 10:55:36 +0530

    Update ForEach Loop

--- Loops/Foreach.java
+++ Loops/Foreach.java
@@ -1,3 +1,4 @@
+package loops;
 /* A program to find average of nos 1,4,2,6,7
  * Using For Each loops
  * */
@@ -5,14 +6,14 @@
 class Foreach 
 {
 	public static void main(String args[]){
-		 int sum=0,av;
-		 int avg [] = {1,4,2,6,7};
-		 for(int i : avg){
-			 sum +=i;
+		int sum=0,av;
+		int avg [] = {1,4,2,6,7};
+		for(int i : avg){
+			sum +=i;
 
-		 }
-		 av = sum/avg.length;
-		 System.out.println("Average is "+av);
+		}
+		av = sum/avg.length;
+		System.out.println("Average is "+av);
 
 	}
 }
