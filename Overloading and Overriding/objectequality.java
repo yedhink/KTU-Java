413b693c92454e70caec8427e44499bab01b5a99
yedhink <yedhin1998@gmail.com>
2018-06-21 17:23:26 +0530

    21 Jun 18 : Create/Update new files

--- Overloading and Overriding/objectequality.java
+++ Overloading and Overriding/objectequality.java
@@ -6,7 +6,7 @@ class objectequality {
     public static void main(String args[]) {
         demo obj1 = new demo();
         demo obj2 = new demo();
-        System.out.println(obj1.equals(obj2));
+        System.out.println((obj1.a).equals(obj2.a));
         System.out.println((obj1==obj2));
     }
 }
