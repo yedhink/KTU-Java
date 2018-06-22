1cfae9d4d749b97bfd78a4e8ec19ec22d0cec40b
yedhink <yedhin1998@gmail.com>
2018-06-22 06:44:40 +0530

    22 Jun 18 : Create/Update new files

--- Misc/strings.java
+++ Misc/strings.java
@@ -4,6 +4,6 @@ class strings {
         StringBuffer sb = new StringBuffer(s1);
         System.out.println(sb);
         System.out.println(sb.reverse());
-        s1 = sb.reverse();
+        s1 = sb.reverse().toString();
     }
 }
