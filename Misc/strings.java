3589ad7bd6adca1fd410f0490f2b392d969cedd1
yedhink <yedhin1998@gmail.com>
2018-06-22 06:42:59 +0530

    22 Jun 18 : Create/Update new files

--- Misc/strings.java
+++ Misc/strings.java
@@ -1,6 +1,6 @@
 class strings {
     public static void main(String args[]) {
-        string s1 = "abc";
+        String s1 = "abc";
         StringBuffer sb = new StringBuffer(s1);
         System.out.println(sb);
     }
